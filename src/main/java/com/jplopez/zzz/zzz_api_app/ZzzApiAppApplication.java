7687cd6c5af827d4030d41117ee7d5180ccf94e8
Juan Pablo Lopez <eclectic.electric@gmail.com>
2024-10-23 16:40:03 -0700

    first commit

---
+++ src/main/java/com/jplopez/zzz/zzz_api_app/ZzzApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.jplopez.zzz.zzz_api_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzzApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzzApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jplopez/zzz/zzz_api_app/ZzzApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jplopez.zzz.zzz_api_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzzApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
